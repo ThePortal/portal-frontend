65e35d0939563f237a4a1b4f0089894e07a823da
ThePortal <oik6716@hotmail.com>
2014-09-21 16:20:17 +0200

    fixed structure

--- app/app/app.js
+++ app/app/app.js
@@ -7,7 +7,7 @@ portalApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/home', {
-        templateUrl: 'templates/home/home.tpl.html',
+        templateUrl: 'app/home/home.tpl.html',
         controller: 'HomeCtrl'
       }).
       otherwise({
